342e9f634d67d0b91dc6547883fabea05761afc3
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-02-27 15:09:24 -0300

    Add files via upload

---
+++ ex001.c
@@ -0,0 +1,21 @@
+//Biblioteca
+//std-i-o
+//std = standard(padrão)
+//i = input(entrada)
+//o = output(saída)
+
+
+//Função principal
+#include <stdio.h>
+
+
+int main(){
+	
+	//comando para mostrar na tela
+	printf("Olá, mundo!");
+	
+	//informar que o programa terminou
+	return 0;
+}
+
+
