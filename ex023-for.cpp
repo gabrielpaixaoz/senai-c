b9c8d5410c3ecf07771809fafdac63837b779c31
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-03-07 13:41:05 -0300

    Add files via upload

---
+++ ex023-for.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+
+int main(){
+	int i;
+	int j;
+	for(i = 1; i<=10;i++){
+		printf("%d \n",i);
+	}
+	
+	for(j = 10;j>=1;j--){
+		printf("%d \n",j);
+	}
+	return 0;
+	
+}
+
