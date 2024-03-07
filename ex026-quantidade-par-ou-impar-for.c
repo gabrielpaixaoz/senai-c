ba78b327713494e49f807360f643e5444236fb77
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-03-07 16:06:47 -0300

    Update and rename Untitled1.c to ex026-quantidade-par-ou-impar-for.c

--- ex026-quantidade-par-ou-impar-for.c
+++ ex026-quantidade-par-ou-impar-for.c
@@ -9,7 +9,7 @@ int main(){
 	int i, nota, numeroPar = 0,numeroImpar = 0;
 	
 	for (i = 1; i <= 5;i++){
-		printf("Digite a %iª nota: ",i);
+		printf("Digite a %iÂª nota: ",i);
 		scanf("%i",&nota);
 		if(nota % 2 == 0){
 			numeroPar = numeroPar + 1;
