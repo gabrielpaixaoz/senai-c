43560cba038298f88aab48101c26a75e58d93577
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-03-12 16:06:55 -0300

    Add files via upload

---
+++ teste.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "");
+	
+	int idade = 1, qtdMA5000, qtdPessoa = 0, resposta = 0;
+	float salario, mediaGrupo = 0, somaGrupo;
+	char sexo;
+	
+	printf("1 OU 2?");
+	scanf("%i",&resposta);
+	
+		while(resposta == 1){
+		printf("Digite a idade da %dª Pessoa: ",qtdPessoa + 1);
+		scanf("%i",&idade);
+		
+		fflush(stdin);
+		
+		printf("Sexo: ");
+		scanf("%c",&sexo);
+		
+		fflush(stdin);
+
+		printf("Salário: ");
+		scanf("%f",&salario);
+		
+		if(idade > 0){
+			somaGrupo - somaGrupo + salario;
+			qtdPessoa = qtdPessoa + 1;
+		}
+		
+		printf("1 OU 2?");
+	scanf("%i",&resposta);
+		
+	}
+
+	
+		printf("media: %f",mediaGrupo);
+
+	}
+	
+	
+	
+
+
+
+
+
+
+
+/*Foi feita uma pesquisa entre os habitantes de uma região.
+Foram coletados os dados de idade,sexo(M/F) e salário.
+faça um algoritmo que informe:
+
+a) a média de salário do grupo;
+b) maior e menor idade do grupo;
+c) quantidade de mulheres com salário a partir de R$ 5.000,00.
+
+Crie um menu com duas opções.
+
+Código | Descrição
+1      | Adicionar pessoa
+2      | Exibir resultados e sair
+
+O final da leitura de dados se dará com quando o usuário digitar o número código 2. 
+*/
+
