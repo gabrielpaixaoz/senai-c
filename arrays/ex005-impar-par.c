d7a727eb1095ad915335c63786d2057d4996f4b8
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-03-15 16:09:49 -0300

    Create ex005-impar-par.c

---
+++ arrays/ex005-impar-par.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#define SIZE 6
+
+
+int main(){
+	setlocale(LC_ALL, "");
+	
+	int numero[SIZE],numeroPar = 0, numeroImpar = 0;
+	int i;
+	
+	for(i = 0; i < SIZE; i++){
+		do{
+		printf("Digite o %iº número: ",i + 1);
+		scanf("%i",&numero[i]);
+		
+		if(numero[i] % 2 == 0){
+			numeroPar++;
+		}else{
+			numeroImpar++;
+		}
+		
+		}while(numeroPar>9999);
+		
+	}
+	
+		printf("Numeros Pares: %i \n",numeroPar);
+		printf("Numeros Impares: %i \n",numeroImpar);
+	
+	}
+	
+
+	
+
+
+/*if(idade > maiorIdade){
+			maiorIdade = idade;
+		}
+		
+		if(idade < menorIdade){
+			menorIdade = idade;
+		}*/
