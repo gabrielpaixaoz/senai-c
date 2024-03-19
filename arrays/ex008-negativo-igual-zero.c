7fa33897374e973b4830d119b142ea1ae962d454
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-03-19 15:42:24 -0300

    Create ex008-negativo-igual-zero.c

---
+++ arrays/ex008-negativo-igual-zero.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#define SIZE 5
+
+
+int main(){
+	setlocale(LC_ALL, "");
+	int numero[SIZE];
+	int i = 0;
+	
+	for(i = 0; i < SIZE;i++){
+		printf("Digite o %iº número: ", i +1);
+		scanf("%i",&numero[i]);
+		
+		if(numero[i] < 0){
+			numero[i] = numero[i] - numero[i];
+		}
+}
+
+for(i = 0; i < SIZE; i++){
+	printf("%iº Número: %i \n", i + 1,numero[i]);
+}
+
+}
+
+	
