39f409364de8509a6280e996e1100a792689a77a
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-03-19 16:16:04 -0300

    Create teste.c

---
+++ arrays/teste.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#define SIZE 6
+
+
+int main(){
+	setlocale(LC_ALL, "");
+	
+	int numero[SIZE];
+	int i;
+	
+	
+	for(i = 0; i < SIZE; i++){
+	do{
+		printf("Digite o %iº número: ",i + 1);
+		scanf("%i",&numero[i]);
+		
+	}while(numero[i] < 0 || numero[i] % 2 != 0);
+}
+
+for(i = 0; i > SIZE; i++){
+	printf("%iº Número: %i \n",i + 1,numero[i]);
+}
+
+
+
+}
