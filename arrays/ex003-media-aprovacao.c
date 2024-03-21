dcc7f6602370aec562083365a1508646d2440fd2
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-03-21 16:53:46 -0300

    Update ex003-media-aprovacao.c

--- arrays/ex003-media-aprovacao.c
+++ arrays/ex003-media-aprovacao.c
@@ -1,48 +1,94 @@
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
-#define SIZE 4
-
+#define SIZE 5
 
 int main(){
 	setlocale(LC_ALL, "");
 	
-
-	float media, soma = 0, numeros[SIZE];
-	char resultado[200];
+	
+	int numero[SIZE],qtdPositivo = 0,qtdNegativo = 0,qtdPar = 0,qtdImpar = 0, qtdNumero = 0,maiorNumero = 0, menorNumero = 999,mediaPar = 0,mediaImpar = 0,somaImpar = 0,somaPar = 0;;
 	int i;
 	
+
 	
-	for(i = 0; i < SIZE; i++){
-	do{
-		printf("Elemento %i: ", i + 1);
-		scanf("%f",&numeros[i]);
+	
+	for(i = 0; i < SIZE;i++){
+		printf("Digite o %iº Número: ",i+1);
+		scanf("%i",&numero[i]);
+		qtdNumero++;
 		
-	}while(numeros[i] < 0 || numeros[i] > 10);
-	
-	soma += numeros[i];
-
+		if(numero[i] % 2 == 0){
+			qtdPar++;
+			somaPar += numero[i];
+		}else{
+			qtdImpar++;
+			somaImpar += numero[i];
+		}
+		
+		numero[i] > 0 ? qtdPositivo++ : qtdNegativo++;
+		
+		if (numero[i] > maiorNumero){
+			maiorNumero = numero[i];
+		}
+		if(numero[i] < menorNumero){
+			menorNumero = numero[i];
+		}
+		
+		
+		mediaPar = somaPar/qtdPar;
+		mediaImpar = somaImpar/qtdImpar;
 	}
 	
-	for(i = 0; i < SIZE; i++){
-		printf("Nota %i: %.1f\n", i+1,numeros[i]);
-	}
-	
-	
-	
-	
-	media = soma / SIZE;
-	
-	
-	if(media >= 7){
-		strcpy(resultado,"Aprovado! \n");
-	}else if(media >= 5){
-		strcpy(resultado,"Recuperação! \n");
-	}else{
-		strcpy(resultado,"Reprovado! \n");
-	}
 
+	
+	printf("Quantidade de pares: %i \n",qtdPar);
+	printf("Quantidade de ímpares: %i \n", qtdImpar);
+	printf("Quantidad de negativos: %i \n",qtdNegativo);
+	printf("Quantidade de números positivos: %i \n",qtdPositivo);
+	printf("Quantidade de números inseridos: %i\n",qtdNumero);
+	printf("Maior número: %i \n",maiorNumero);
+	printf("Menor número: %i \n",menorNumero);
+	printf("A média dos números pares é: %i \n",mediaPar);
+	printf("A média dos números impares é : %i \n",mediaImpar);
+	
+	
+	
+	
+	
+	
 
-	printf("Resultado: %s \n",resultado);
-	printf("MEDIA: %.1f",media);
 }
+
+
+
+
+
+	/*do{
+		printf("Digite o %iº Número: ",qtdNumero + 1);
+		scanf("%i",&numero);
+		qtdNumero++;
+		
+		
+		if(numero > 0 && numero % 2 == 0){
+			qtdPositivoPar++;
+		}else if(numero > 0 && numero % 2 != 0){
+			qtdPositivoImpar++;
+		}
+		
+		
+		numero > 0 ? qtdPositivo++ : qtdNegativo++;
+		
+		if (numero > maiorNumero){
+			maiorNumero = numero;
+		}
+		if(numero < menorNumero){
+			menorNumero = numero;
+		}
+		
+		
+		
+		
+		
+	}while(numero != 0);
+	*/
