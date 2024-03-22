db9ac182c2919e6ecd6d64f447afb2c61d3d8e7b
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-03-22 15:41:02 -0300

    Create ex003-for-resultado.c

---
+++ matrizes/ex003-for-resultado.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#define SIZE 5
+
+int main(){
+	setlocale(LC_ALL, "");
+	int numeros[2][2];
+	int i,j;
+	
+	printf("Digite o elementos para matriz: \n");
+	for(i=0;i<2;i++){
+		for(j = 0; j<2;j++){
+			printf("Elemento da linha %i coluna %i: ", i + 1, j + 1);
+			scanf("%i",&numeros[i][j]);
+		}
+	}
+	
+	for(i = 0; i < 2;i++){
+		for(j = 0;j < 2;j++){
+			printf("Elemento da linha %i coluna %i: %i \n",i + 1,j + 1,numeros[i][j]);
+		}
+	}
+
+
+}
+
+
