6ae69b6a2fed73cf6ae5699aba36ea24ac84ddb5
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-03-22 15:37:46 -0300

    Update ex001-inicio.c

--- matrizes/ex001-inicio.c
+++ matrizes/ex001-inicio.c
@@ -6,19 +6,18 @@
 int main(){
 	setlocale(LC_ALL, "");
 	int numeros[2][2];
-	int i,j;
 	
-	printf("Digite o elementos para matriz: \n");
-	for(i=0;i<2;i++){
-		for(j = 0; j<2;j++){
-			printf("Elemento da linha %i coluna %i", i + 1, j + 1);
-			scanf("%i",&numeros[i][j]);
-		}
-	}
+	numeros[0][0] = 1;
+	numeros[0][1] = 2;
+	numeros[1][0] = 3;
+	numeros[1][1] = 4;
 	
-	printf("%i",numeros[0][0]);
+	
+	printf("Elemento 1: %d \n",numeros[0][0]);
+	printf("Elemento 1: %d \n",numeros[0][1]);
+	printf("Elemento 1: %d \n",numeros[1][0]);
+	printf("Elemento 1: %d \n",numeros[1][1]);
 
 }
-
 
 
